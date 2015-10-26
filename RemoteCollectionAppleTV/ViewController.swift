022ebf3b4eacd3735d4395811694b6c02cd9e76e
tiagomartinho <tiago.martinho@xpeppers.com>
2015-10-26 19:30:21 +0100

    Initial Commit

---
+++ RemoteCollectionAppleTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemoteCollectionAppleTV
+//
+//  Created by Tiago Martinho on 26/10/15.
+//  Copyright © 2015 Tiago Martinho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
