47bbf0373caec230c9ca449f6c05a541302230d6
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-29 21:53:30 +0100

    first implementation of cell view

--- RemoteCollectionAppleTV/ItemViewCell.swift
+++ RemoteCollectionAppleTV/ItemViewCell.swift
@@ -6,25 +6,19 @@ class ItemViewCell: UICollectionViewCell {
     
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var image: UIImageView!
-
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    
     static let reuseIdentifier = "ItemViewCell"
-
+    
     var item:Item? {
         didSet{
-            title?.text = item?.name
-            image?.sd_setImageWithURL(item?.iconURL) { [unowned self] imageFromURL, _, _, _ in
-                self.setImageTint(imageFromURL)
-            }
+            title?.text = item?.title
+            name?.text = item?.name
+            image?.sd_setImageWithURL(item?.imageURL)
+            logo?.sd_setImageWithURL(item?.iconURL)
         }
-    }
-    
-    func setImageTint(imageFromURL:UIImage){
-        var tintedImage = imageFromURL.imageWithRenderingMode(.AlwaysTemplate)
-        UIGraphicsBeginImageContextWithOptions(imageFromURL.size, false, tintedImage.scale)
-        self.item?.color.set()
-        tintedImage.drawInRect(CGRectMake(0, 0, imageFromURL.size.width, imageFromURL.size.height))
-        tintedImage = UIGraphicsGetImageFromCurrentImageContext()
-        self.image.image = tintedImage
     }
     
     // MARK: Initialization
@@ -33,14 +27,21 @@ class ItemViewCell: UICollectionViewCell {
         super.awakeFromNib()
         image.adjustsImageWhenAncestorFocused = true
         image.clipsToBounds = false
-        title.alpha = 0.0
+        initialViewsState()
     }
     
     // MARK: UICollectionReusableView
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        initialViewsState()
+    }
+    
+    func initialViewsState(){
         title.alpha = 0.0
+        name.alpha = 0.0
+        logo.alpha = 0.0
+        containerView.backgroundColor = nil
     }
     
     // MARK: UIFocusEnvironment
@@ -48,7 +49,9 @@ class ItemViewCell: UICollectionViewCell {
     override func didUpdateFocusInContext(context: UIFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator) {
         coordinator.addCoordinatedAnimations({ [unowned self] in
             self.title.alpha = self.focused ? 1.0 : 0.0
-            self.image?.backgroundColor = self.focused ? self.item?.color : nil
+            self.name.alpha = self.focused ? 1.0 : 0.0
+            self.logo.alpha = self.focused ? 1.0 : 0.0
+            self.containerView.backgroundColor = self.focused ? self.item?.color.colorWithAlphaComponent(0.5) : nil
             }, completion: nil)
     }
 }
