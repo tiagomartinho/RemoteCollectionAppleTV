c28c61d7420468bfa7ae5b12c21b18f6f95d7b4b
tiagomartinho <tiago.martinho@xpeppers.com>
2016-03-01 14:11:24 +0100

    set background color

--- RemoteCollectionAppleTV/ItemViewCell.swift
+++ RemoteCollectionAppleTV/ItemViewCell.swift
@@ -9,6 +9,7 @@ class ItemViewCell: UICollectionViewCell {
     @IBOutlet weak var logo: UIImageView!
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var contentBackgroundView: UIView!
     
     static let reuseIdentifier = "ItemViewCell"
     
@@ -38,20 +39,24 @@ class ItemViewCell: UICollectionViewCell {
     }
     
     func initialViewsState(){
-        title.alpha = 0.0
-        name.alpha = 0.0
-        logo.alpha = 0.0
-        containerView.backgroundColor = nil
+//        title.alpha = 0.0
+//        name.alpha = 0.0
+//        logo.alpha = 0.0
+        image.alpha = 0.75
+        containerView.alpha = 0.0
+        contentBackgroundView.backgroundColor = nil
     }
     
     // MARK: UIFocusEnvironment
     
     override func didUpdateFocusInContext(context: UIFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator) {
         coordinator.addCoordinatedAnimations({ [unowned self] in
-            self.title.alpha = self.focused ? 1.0 : 0.0
-            self.name.alpha = self.focused ? 1.0 : 0.0
-            self.logo.alpha = self.focused ? 1.0 : 0.0
-            self.containerView.backgroundColor = self.focused ? self.item?.color.colorWithAlphaComponent(0.5) : nil
+//            self.title.alpha = self.focused ? 1.0 : 0.0
+//            self.name.alpha = self.focused ? 1.0 : 0.0
+//            self.logo.alpha = self.focused ? 1.0 : 0.0
+            self.image.alpha = self.focused ? 1.0 : 0.75
+            self.containerView.alpha = self.focused ? 1.0 : 0.0
+            self.contentBackgroundView.backgroundColor = self.focused ? self.item?.color.colorWithAlphaComponent(0.75) : nil
             }, completion: nil)
     }
 }
